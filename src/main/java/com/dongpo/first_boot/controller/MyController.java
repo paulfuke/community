6143fe348a0e0d160918520d6635e1a44b1bbca1
paulfuke <1520436215@qq.com>
2019-12-20 10:35:11 +0800

    add index and resource

--- src/main/java/com/dongpo/first_boot/controller/MyController.java
+++ src/main/java/com/dongpo/first_boot/controller/MyController.java
@@ -1,13 +1,14 @@
 package com.dongpo.first_boot.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MyController {
 
-    @RequestMapping("/helloTest")
+    @GetMapping("/")
     public String testRequest(){
-        return "hello";
+        return "index";
     }
 }
